8c1c753fe91c5d28dca7ee4608b8fe1092f93031
XanderTG <ajsteim6@gmail.com>
2020-11-06 07:19:55 -0700

    First commit

---
+++ quote-deck/src/mock-data.js
@@ -0,0 +1,44 @@
+let mock = [
+  {
+    id: 1,
+    author: "Winston Churchill",
+    Quote: "If we open a quarrel between past and present, we shall find that we have lost the future.",
+    Topic: "Advice"
+  },
+  {
+    id: 2,
+    author: "C. S. Lewis",
+    Quote: "Courage is not simply one of the virtues, but the form of every virtue at the testing point.",
+    Topic: "Encouragement"
+  },
+  {
+    id: 3,
+    author: "Trenton Lee Stewart",
+    Quote: "\"Remember the white knight.\" Though it seemed so long ago, he well remembered their conversation about the chess problem. The white knight had made a move, changed his mind, and started over. \'And do you believe this was a good move?\" Mr. Benedict had asked. \"No, sir,\" Reynie had answered. \"Why, then, do you think he made it?\" And Reynie had replied, \"Perhaps because he doubted himself.\"",
+    Topic: "Encouragement"
+  },
+  {
+    id: 4,
+    author: "Ray Bradbuyr",
+    Quote: "I hate all politics. I don't like either political party. One should not belong to them - one should be an individual, standing in the middle. Anyone that belongs to a party stops thinking.",
+    Topic: "Advice"
+  },
+  {
+    id: 5,
+    author: "Dallin H. Oaks",
+    Quote: "Those who govern their thoughts and actions solely by the principles of liberalism or conservatism or intellectualism cannot be expected to agree with all of the teachings of the gospel of Jesus Christ. As for me, I find some wisdom in liberalism, some wisdom in conservatism, and much truth in intellectualism—but I find no salvation in any of them.",
+    Topic: "Religion"
+  },
+  {
+    id: 6,
+    author: "Gordon B. Hinckley",
+    Quote: "Try a little harder to be a little better.",
+    Topic: "Encouragement"
+  },
+  {
+    id: 7,
+    author: "Gordon B. Hinckley",
+    Quote: "True love is not so much a matter of romance as it is a matter of anxious concern for the well-being of one's companion.",
+    Topic: "Relationships"
+  }
+]
